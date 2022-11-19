80bc477008d95673e6c91059bee6dd1fbbd26538
rustzz <rustzz.devel@gmail.com>
2022-11-19 23:20:27 +0300

    remove warnings; colorful warnings

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/rustzz/kata-calc
 
 go 1.19
+
+require (
+	github.com/withmandala/go-log v0.1.0 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/term v0.2.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -5,38 +5,38 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	golog "github.com/withmandala/go-log"
 )
 
 func main() {
+	logger := golog.New(os.Stderr).WithColor()
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Println("Введите выражение: ")
 		line, _ := reader.ReadString('\n')
 		splittedLine := strings.Split(line, " ")
-		for index, _ := range splittedLine {
+		for index := range splittedLine {
 			splittedLine[index] = strings.TrimSpace(splittedLine[index])
 		}
-
 		if len(splittedLine) != INPUT_DATA {
-			fmt.Println("Введенные данные не удовлетворяет условию: 2 числа и знак арифметической операции в одну строку")
+			logger.Warn("Введенные данные не удовлетворяет условию: 2 числа и знак арифметической операции в одну строку")
 			continue
 		}
-
 		if isArabic(splittedLine[ORDINAL_FIRST_NUMBER]) && isArabic(splittedLine[ORDINAL_SECOND_NUMBER]) {
-			fmt.Println("dawdawdaw")
 			fmt.Println(performAnAction(splittedLine[ORDINAL_FIRST_NUMBER], splittedLine[OPERATION_SIGN], splittedLine[ORDINAL_SECOND_NUMBER]))
 			continue
 		}
 		if !isArabic(splittedLine[ORDINAL_FIRST_NUMBER]) && !isArabic(splittedLine[ORDINAL_SECOND_NUMBER]) {
 			var resultArabic int = performAnAction(romeToArabic(splittedLine[ORDINAL_FIRST_NUMBER]), splittedLine[OPERATION_SIGN], romeToArabic(splittedLine[ORDINAL_SECOND_NUMBER]))
 			if resultArabic < 1 {
-				fmt.Println("В римской системе нет чисел меньше 1")
+				logger.Warn("В римской системе нет чисел меньше 1")
 			}
 			fmt.Println(arabicToRome(resultArabic))
 			continue
 		}
 		if !(isArabic(splittedLine[ORDINAL_FIRST_NUMBER]) && isArabic(splittedLine[ORDINAL_SECOND_NUMBER])) {
-			fmt.Println("Используются одновременно разные системы счисления")
+			logger.Warn("Используются одновременно разные системы счисления")
 		}
 	}
 }
